fd60dea3d7e366844e56f3096fa367ba4a9a1b5c
GitHub Action <action@github.com>
2022-04-23 09:52:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/lihuu/testplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/lihuu/testplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.lihuu.testplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lihuu.testplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/lihuu/testplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/lihuu/testplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.lihuu.testplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lihuu.testplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
